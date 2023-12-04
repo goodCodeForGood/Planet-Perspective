273ac5342033c64c6999a9eab811327e6dbf1ea9
Micaela <115068478+mica411@users.noreply.github.com>
2023-12-03 21:48:53 -0600

    Create Resources.js

---
+++ planet-pers/src/components/Resources.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+export default function Resources() {
+    return (
+<div id="getInvContainer">
+<div id="dailyLifeSuggestions">
+
+  
+    <h2>Daily Life Suggestions for Climate Change</h2>
+    <ul>
+        <li>Reduce, Reuse, Recycle: Minimize waste by following the three Rs. Recycle paper, plastic, glass, and electronics.</li>
+        <li>Conserve Energy: Turn off lights and appliances when not in use. Use energy-efficient appliances and consider renewable energy sources.</li>
+        <li>Use Sustainable Transportation: Walk, bike, carpool, or use public transport to reduce carbon emissions from personal vehicles.</li>
+        <li>Conserve Water: Fix leaks, use water-efficient appliances, and collect rainwater for gardening.</li>
+        <li>Eat Sustainably: Choose local and seasonal foods to reduce the carbon footprint of food transportation. Reduce meat consumption and support sustainable farming practices.</li>
+        <li>Plant Trees: Trees absorb carbon dioxide and provide oxygen. Consider planting trees in your community or supporting reforestation projects.</li>
+        <li>Educate Others: Share information about climate change and sustainable practices with friends, family, and colleagues.</li>
+        <li>Minimize Single-Use Plastics: Opt for reusable items and reduce your reliance on single-use plastics to decrease environmental pollution.</li>
+        <li>Support Sustainable Brands: Choose products from companies committed to eco-friendly and ethical practices.</li>
+        <li>Conscious Water Usage: Be mindful of water consumption in daily activities, such as washing dishes efficiently and taking shorter showers.</li>
+    </ul>
+  
+</div>
+
+<div id="outreachOrganizations">
+    <h2>Outreach Organizations</h2>
+    <ul>
+        <li>
+            <strong>World Wildlife Fund (WWF)</strong>: WWF works to protect the world's most vulnerable species and ecosystems. Impact: Conservation of endangered species, habitat preservation, and sustainable development.
+        </li>
+        <li>
+            <strong>350.org</strong>: 350.org focuses on addressing climate change by reducing carbon dioxide levels in the atmosphere. Impact: Advocacy for renewable energy, divestment from fossil fuels, and global climate awareness campaigns.
+        </li>
+        <li>
+            <strong>The Climate Reality Project</strong>: Founded by Al Gore, this organization educates and advocates for solutions to the climate crisis. Impact: Climate leadership training, public awareness campaigns, and policy advocacy for sustainable practices.
+        </li>
+    </ul>
+</div>
+    </div>
+)}
