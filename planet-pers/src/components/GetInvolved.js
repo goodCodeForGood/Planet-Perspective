804d2823670b4987fcab1aefcf40a096d55c240e
Micaela <115068478+mica411@users.noreply.github.com>
2023-12-03 21:50:02 -0600

    Update GetInvolved.js

--- planet-pers/src/components/GetInvolved.js
+++ planet-pers/src/components/GetInvolved.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+//need to to research further on how to get submit button to work
+
 const Form = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
