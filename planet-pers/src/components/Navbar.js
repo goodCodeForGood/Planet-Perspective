c4432951eb6539eb0131e392428d5eb995329cef
goodCodeForGood <sahitihibane6@gmail.com>
2023-12-05 01:32:01 -0800

    Add calculator to navigation bar

--- planet-pers/src/components/Navbar.js
+++ planet-pers/src/components/Navbar.js
@@ -3,14 +3,13 @@ import React from "react"
 export default function Navbar() {
     return (
       
-
 <div class="main">
 <nav>
     <div class="top-nav">
         <a href="About.js">About</a>
             <a href="Quiz.js">Quiz</a>
             <a href="GetInvolved.js">Get Involved</a>
-        
+            <a href="Calculator.js">Carbon Footprint Calculator</a>
     </div>
 </nav>
 </div>
