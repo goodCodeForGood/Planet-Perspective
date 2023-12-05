2676ffd372bbdfc6eb53007961d7b7ce4853a144
goodCodeForGood <sahitihibane6@gmail.com>
2023-12-05 00:22:10 -0800

    Add title for carbon footprint calculator

--- planet-pers/src/components/Calculator.js
+++ planet-pers/src/components/Calculator.js
@@ -2,15 +2,15 @@ import React from "react"
 
 export default function Calculator() {
     return (
-        <iframe 
-            width="710" 
-            height="1300" 
-            frameborder="0" 
-            marginwidth="0" 
-            marginheight="0" 
-            scrolling="no" 
+        <><h1>Carbon Footprint Calculator</h1><iframe
+            width="710"
+            height="1300"
+            frameborder="0"
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
             src="https://calculator.carbonfootprint.com/calculator.aspx">
-        </iframe>
+        </iframe></>
     );
 
 }
